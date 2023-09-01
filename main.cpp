a1be35f533c108698ae496e2a954fa21386c342d
XChanitoX <christian.salazar.a@utec.edu.pe>
2023-08-31 20:52:20 -0500

    Reto terminado

--- main.cpp
+++ main.cpp
@@ -1,49 +1,72 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <sstream>
 
 using namespace std;
 
-int main() {
+void Merge(vector<string> horasIngreso, vector<string> horasSalida, vector<string> &horasResultado){
+    int tamanho = horasIngreso.size();
+    
+    horasResultado.push_back(horasIngreso[0]);
+    for (int i = 0; i < tamanho-1; i++){
+        if (horasSalida[i] != horasIngreso[i+1]){
+            horasResultado.push_back(horasSalida[i]);
+            horasResultado.push_back(horasIngreso[i+1]);
+        }
+    }
+    horasResultado.push_back(horasSalida[tamanho-1]);
+}
 
-    int cantidadHoras;
-    string horaAux;
-    vector<string> horas(0);
+int main(){
 
-    do {
-        cout << "Ingrese la cantidad de intervalos a ingresar: " << endl;
-        cin >> cantidadHoras;
-    } while (cantidadHoras <= 0);
+    vector<string> horasResultado(0);
+    vector<string> horasIngreso(0);
+    vector<string> horasSalida(0);
 
-    for (int i = 0; i < cantidadHoras * 2; i++) {
-        if (i % 2 == 0)
-            cout << "Ingrese la hora de ingreso: " << endl;
-        else
-            cout << "Ingrese la hora de salida: " << endl;
+    string nombreArchivo;
+    cout << "Ingrese el nombre del archivo CSV: ";
+    cin >> nombreArchivo;
 
-        cin >> horaAux;
-        bool horaRepetida = false;
-        for (int j = 0; j < horas.size(); j++) {
-            if (horaAux == horas[j]) {
-                horaRepetida = true;
-                horas.erase(horas.begin() + j);
-                break;
+    ifstream archivo(nombreArchivo);
+    if (!archivo.is_open()) {
+        cout << "No se pudo abrir el archivo." << endl;
+        return 1;
+    }
+
+    string primeraLinea;
+    getline(archivo, primeraLinea);
+
+    string linea;
+    while (getline(archivo, linea)) {
+        stringstream ss(linea);
+        string horaAux;
+
+        bool esHoraIngreso = true;
+        while (getline(ss, horaAux, ',')) {
+            if (esHoraIngreso){
+                horasIngreso.push_back(horaAux);
+                esHoraIngreso = false;
+            } else {
+                horasSalida.push_back(horaAux);
+                esHoraIngreso = true;
             }
         }
 
-        if (!horaRepetida) {
-            horas.push_back(horaAux);
-        }
     }
 
-    ofstream archivoCSV("horas.csv");
+    archivo.close();
+
+    Merge(horasIngreso, horasSalida, horasResultado);
+
+    ofstream archivoCSV("tabla2.csv");
     if (archivoCSV.is_open()) {
         archivoCSV << "INGRESO,SALIDA\n";
-        for (int i = 0; i < horas.size(); i += 2) {
-            archivoCSV << horas[i] << "," << horas[i + 1] << "\n";
+        for (int i = 0; i < horasResultado.size(); i += 2) {
+            archivoCSV << horasResultado[i] << "," << horasResultado[i + 1] << "\n";
         }
         archivoCSV.close();
-        cout << "Archivo CSV 'horas.csv' creado exitosamente." << endl;
+        cout << "Archivo CSV 'tabla2.csv' creado exitosamente." << endl;
     } else {
         cout << "No se pudo crear el archivo CSV." << endl;
     }
